60ac2dc754d69b33d40f94dbbdb88d776d8bfa4d
agustnlee <lyoonsoo25@gmail.com>
2026-05-25 19:03:51 -0300

    feat: EscaladorImagen para cambiar el tamaño de BufferedImage

--- src/main/java/com/Sokoban_No_Presenta/vista/utils/EscaladorImagen.java
+++ src/main/java/com/Sokoban_No_Presenta/vista/utils/EscaladorImagen.java
@@ -1,5 +1,20 @@
 package vista.utils;
 
-public class EscaladorImagen {
-    
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+public final class EscaladorImagen {
+
+    private EscaladorImagen() {}
+
+    public static BufferedImage escalar(BufferedImage src, int ancho, int alto) {
+        BufferedImage resultado = new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = resultado.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                           RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(src, 0, 0, ancho, alto, null);
+        g.dispose();
+        return resultado;
+    }
 }
