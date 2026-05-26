216272e47b5e94eba18b016b934dec1de2445aff
agustnlee <lyoonsoo25@gmail.com>
2026-05-25 21:12:42 -0300

    feat: Incorporado mvn wrapper para poder ejecutar el proyecto sin instalar mvn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import vista.MainVista;
+
 public class Main {
 
     // TODO: Importar e instanciar Main modelo vista y controlador
     
-    public static int main(String[] args) {
-        return 0;
+    public static void main(String[] args) {
+        MainVista mainVista = new MainVista();
     }
 }
 
