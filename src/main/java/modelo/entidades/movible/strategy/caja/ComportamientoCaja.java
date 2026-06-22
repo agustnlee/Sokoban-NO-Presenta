c36a57ec9343feb62ae5aab593fa2bfb1e41c305
agustnlee <lyoonsoo25@gmail.com>
2026-06-22 17:54:31 -0300

    feat: Implementacion Strategy contrato ComportamientoCaja y ComportamientoEntidadMovible

--- src/main/java/modelo/entidades/movible/strategy/caja/ComportamientoCaja.java
+++ src/main/java/modelo/entidades/movible/strategy/caja/ComportamientoCaja.java
@@ -1,5 +1,12 @@
 package modelo.entidades.movible.strategy.caja;
 
+import modelo.entidades.Tablero;
+import modelo.entidades.TipoEntidad;
+import modelo.entidades.movible.Caja;
+
 public interface ComportamientoCaja {
-    
+    void reaccionarLlegada(Caja c, Tablero tablero);
+    boolean cuentaParaVictoria();
+    boolean estaEnEstadoDerrota();
+    TipoEntidad getTipoEntidad();
 }
--- src/main/java/modelo/entidades/movible/strategy/entidadmovible/ComportamientoEntidadMovible.java
+++ src/main/java/modelo/entidades/movible/strategy/entidadmovible/ComportamientoEntidadMovible.java
@@ -1,5 +1,9 @@
 package modelo.entidades.movible.strategy.entidadmovible;
 
+import modelo.entidades.Coordenada;
+import modelo.entidades.Direccion;
+import modelo.entidades.Tablero;
+
 public interface ComportamientoEntidadMovible {
-    
+    Coordenada calcularSiguientePosicion(Coordenada posicionActual, Direccion dir, Tablero tablero);
 }
