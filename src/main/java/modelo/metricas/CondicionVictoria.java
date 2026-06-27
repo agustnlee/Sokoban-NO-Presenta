875350d91f894fa0bb3d87d9d491b50bfe4fb123
agustnlee <lyoonsoo25@gmail.com>
2026-06-27 18:27:55 -0300

    feat: Implementacion de condicion derrota y victoria como recorrido de tableros y utilizacion de metodos polimorficos

--- src/main/java/modelo/metricas/CondicionVictoria.java
+++ src/main/java/modelo/metricas/CondicionVictoria.java
@@ -1,5 +1,20 @@
 package modelo.metricas;
 
+import modelo.entidades.Coordenada;
+import modelo.entidades.Tablero;
+import modelo.entidades.movible.EntidadMovible;
+
 public class CondicionVictoria {
-    
+
+    public boolean evaluar(Tablero tablero) {
+        for (int fila = 0; fila < tablero.getFilaMax(); fila++) {
+            for (int columna = 0; columna < tablero.getColumnaMax(); columna++) {
+                EntidadMovible entidad = tablero.getMovible(new Coordenada(fila, columna));
+                if (entidad != null && entidad.cuentaParaVictoria() && !entidad.estaBloqueada()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
