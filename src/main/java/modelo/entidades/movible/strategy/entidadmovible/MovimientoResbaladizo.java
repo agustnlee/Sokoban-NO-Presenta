92041d8a14891e2887873e9c826ae537e8696800
agustnlee <lyoonsoo25@gmail.com>
2026-06-27 13:30:38 -0300

    feat: Implementacion casilla resbaladiza

--- src/main/java/modelo/entidades/movible/strategy/entidadmovible/MovimientoResbaladizo.java
+++ src/main/java/modelo/entidades/movible/strategy/entidadmovible/MovimientoResbaladizo.java
@@ -1,5 +1,13 @@
 package modelo.entidades.movible.strategy.entidadmovible;
 
+import modelo.entidades.Coordenada;
+import modelo.entidades.Direccion;
+import modelo.entidades.Tablero;
+
 public class MovimientoResbaladizo implements ComportamientoEntidadMovible {
-    
+
+    @Override
+    public Coordenada calcularSiguientePosicion(Coordenada posicionActual, Direccion dir, Tablero tablero) {
+        return posicionActual.sumar(dir);
+    }
 }
