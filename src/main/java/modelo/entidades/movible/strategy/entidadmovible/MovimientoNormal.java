ea1c2d34810bd4acb17c0f3663034de7be8bd306
agustnlee <lyoonsoo25@gmail.com>
2026-06-27 02:10:54 -0300

    feat: Implementacion de movimiento normal, simplemente calcular sumando uno

--- src/main/java/modelo/entidades/movible/strategy/entidadmovible/MovimientoNormal.java
+++ src/main/java/modelo/entidades/movible/strategy/entidadmovible/MovimientoNormal.java
@@ -1,5 +1,13 @@
 package modelo.entidades.movible.strategy.entidadmovible;
 
-public class MovimientoNormal {
-    
+import modelo.entidades.Coordenada;
+import modelo.entidades.Direccion;
+import modelo.entidades.Tablero;
+
+public class MovimientoNormal implements ComportamientoEntidadMovible {
+
+    @Override
+    public Coordenada calcularSiguientePosicion(Coordenada posicionActual, Direccion dir, Tablero tablero) {
+        return posicionActual.sumar(dir);
+    }
 }
